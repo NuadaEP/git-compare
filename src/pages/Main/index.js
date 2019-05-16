cd52e9748fa2393c7c42e957a8287b5b3c02d989
Bruno Cardoso <bruno@liveonsolutions.com>
2019-05-16 00:28:37 -0300

    adding the font awesome and insert a spinner when the request begins

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,7 @@ import CompareList from '../../components/CompareList';
 
 export default class Main extends Component {
   state = {
+    loading: false,
     repositoryError: false,
     repositoryInput: '',
     respositories: [],
@@ -17,6 +18,8 @@ export default class Main extends Component {
 
   handleAddRepository = async (event) => {
     event.preventDefault();
+
+    this.setState({ loading: true });
 
     try {
       const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
@@ -32,6 +35,8 @@ export default class Main extends Component {
       });
     } catch (err) {
       this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
     }
   };
 
@@ -46,7 +51,9 @@ export default class Main extends Component {
             value={this.state.repositoryInput}
             onChange={event => this.setState({ repositoryInput: event.target.value })}
           />
-          <button type="submit">OK</button>
+          <button type="submit">
+            {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
         </Form>
 
         <CompareList respositories={this.state.respositories} />
